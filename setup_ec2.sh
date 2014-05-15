75af5b8be2d748905a83435cd28f44e9868a6de1
wangjohn <wangjohn@mit.edu>
2014-05-15 17:40:57 +0000

    Adding setup script for ec2 machines.

---
+++ setup_ec2.sh
@@ -0,0 +1,24 @@
+# Change the system password
+
+passwd
+
+# Symlink all the configuration files
+
+rm ~/.bashrc
+rm ~/.vimrc
+
+ln -s ~/configuration_files/.ackrc ~/.ackrc
+ln -s ~/configuration_files/.bashrc ~/.bashrc
+ln -s ~/configuration_files/.gitconfig ~/.gitconfig
+ln -s ~/configuration_files/.tmux.conf ~/.tmux.conf
+ln -s ~/configuration_files/.vimrc ~/.vimrc
+
+# Installing pathagoen for vimrc
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Install commonly used programs
+
+apt-get install ack-grep
+
